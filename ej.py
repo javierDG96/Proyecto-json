206d5c6e28716bb41bf5f40e38a121f787f4e6df
Javier Domingo Gonzalez <Javi_dg@hotmail.es>
2020-03-05 21:54:30 +0100

    He hecho el 2 ejercicio

--- ej.py
+++ ej.py
@@ -8,10 +8,13 @@ def listar_auto(prueba):
 			autores.append(prueba["tracks"]["items"][i]["track"]["artists"][1]["name"])
 	return set(autores)
 	
-with open("canciones.json") as fichero:
-		prueba = json.load(fichero)	
-		
-		print(listar_auto(prueba))
-	
+def contar_canciones(prueba):
+	canciones=[]
+	for i in range(0,50):
+		canciones.append(prueba["tracks"]["items"][i]["track"]["name"])
+
+	return len(canciones)
+
+
 	
 
